d7475344f87e40a5809674d0f46274d75f942118
Ian Mathaiya <iannmathaiya@gmail.com>
2023-04-24 16:55:56 +0300

    variables commit

---
+++ 01-data-types-variables-methods/variables.js
@@ -0,0 +1,46 @@
+let firstName = 'John'
+let lastName = 'Smith'
+let age = 30
+
+console.log(age)
+
+age = 31
+
+console.log(age);
+
+let score
+
+score = 1
+
+if (true) {
+    score = score +1
+}
+
+console.log(score)
+
+const x = 100
+
+const arr = [1, 2, 3, 4]
+
+arr.push(5)
+
+console.log(arr)
+
+const person = {
+    name: 'Ian'
+}
+
+person.name = 'x33'
+
+person.email = 'x33@gmail.com'
+
+console.log(person)
+
+let a, b, c
+
+const d = 10, e = 20, f = 30
+
+
+console.log(a, b, c)    // returns undefined since variables are not initialized
+
+console.log(d, e, f)
